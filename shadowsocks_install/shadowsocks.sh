5c0b7bd6d74abba86c5167b2db5099dfa316fb89
kennyoysj <291061251@qq.com>
2019-09-24 12:42:52 +0800

    Update shadowsocks.sh

--- shadowsocks_install/shadowsocks.sh
+++ shadowsocks_install/shadowsocks.sh
@@ -246,7 +246,7 @@ download_files(){
     fi
     # Download Shadowsocks init script
     if check_sys packageManager yum; then
-        if ! wget --no-check-certificate https://raw.githubusercontent.com/kennyoysj/teddysunBackup/shadowsocks_install/master/shadowsocks -O /etc/init.d/shadowsocks; then
+        if ! wget --no-check-certificate https://raw.githubusercontent.com/kennyoysj/teddysunBackup/master/shadowsocks_install/shadowsocks -O /etc/init.d/shadowsocks; then
             echo -e "[${red}Error${plain}] Failed to download shadowsocks chkconfig file!"
             exit 1
         fi
